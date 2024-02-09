29a696a905afa161c41e2c7305571dcdb5dcc845
Darpan Kakadia <kakadiadarpan@gmail.com>
2024-02-09 11:49:29 +0100

    change tracing app name

--- shared/fetch-github-stars.ts
+++ shared/fetch-github-stars.ts
@@ -1,19 +1,19 @@
-import { trace } from '@opentelemetry/api'
+import { trace } from "@opentelemetry/api";
 
-export async function fetchGithubStars(repo = 'vercel/next.js') {
+export async function fetchGithubStars(repo = "vercel/next.js") {
   return await trace
-    .getTracer('nextjs-example')
-    .startActiveSpan('fetchGithubStars', async (span) => {
+    .getTracer("sample-app-example")
+    .startActiveSpan("fetchGithubStars", async (span) => {
       try {
         const res = await fetch(`https://api.github.com/repos/${repo}`, {
           next: {
             revalidate: 0,
           },
-        })
-        const data = await res.json()
-        return data.stargazers_count
+        });
+        const data = await res.json();
+        return data.stargazers_count;
       } finally {
-        span.end()
+        span.end();
       }
-    })
+    });
 }
